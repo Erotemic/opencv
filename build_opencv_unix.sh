e76f5d5d23307492a055d355deccd81b16590218
Jason Parham <bluemellophone@gmail.com>
2014-01-07 12:43:31 -0500

    Updated unix script to be compatible with OSX

--- build_opencv_unix.sh
+++ build_opencv_unix.sh
@@ -2,6 +2,6 @@ cd ~/code/opencv
 mkdir ~/code/opencv/build
 cd ~/code/opencv/build
 
-cmake ..
+cmake -G "Unix Makefiles" -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO ..
 make -j9
 sudo make install
