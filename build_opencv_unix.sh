ede2b9998913709991a78cbf2bace2346ac92f13
Jon Crall <crallj@rpi.edu>
2014-01-07 13:52:37 -0500

    Added correct way to get site-packages

--- build_opencv_unix.sh
+++ build_opencv_unix.sh
@@ -2,6 +2,10 @@ cd ~/code/opencv
 mkdir ~/code/opencv/build
 cd ~/code/opencv/build
 
-cmake -G "Unix Makefiles" -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO ..
+# Grab the correct site_packages
+PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
+
+cmake -G "Unix Makefiles" -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO ..
+
 make -j9
 sudo make install
