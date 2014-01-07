eae2c02eb5dde6fa126007bf12dcc164173ea49a
Jason Parham <bluemellophone@gmail.com>
2014-01-07 15:58:32 -0500

    Working on mac script

--- build_opencv_unix.sh
+++ build_opencv_unix.sh
@@ -1,11 +1,28 @@
 cd ~/code/opencv
+rm -rf ~/code/opencv/build
 mkdir ~/code/opencv/build
 cd ~/code/opencv/build
 
-# Grab the correct site_packages
-PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
+# locate libpython
 
-cmake -G "Unix Makefiles" -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO ..
+# Grab the correct site_packages
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	PYTHON_PACKAGES_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages"
+	# PYTHON_LIBRARY="/opt/local/lib/libpython2.7.dylib"
+	# PYTHON_LIBRARY="/usr/lib/libpython2.7.dylib"
+	PYTHON_LIBRARY="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/libpython2.7.dylib"
+	# PYTHON_LIBRARY=""
+	# PYTHON_LIBRARY=""
+	# PYTHON_LIBRARY=""
+	# PYTHON_LIBRARY=""
+
+    cmake -G "Unix Makefiles" -D PYTHON_LIBRARY=$PYTHON_LIBRARY -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO .. 
+    # -D CMAKE_OSX_ARCHITECTURES=x86_64
+    # -D PYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib 
+else
+	PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
+    cmake -G "Unix Makefiles" -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO ..
+fi
 
 make -j9
 sudo make install
