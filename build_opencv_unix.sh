6daadd79b8177f620d83229d15f96f2c5e89d79c
joncrall <crallj@rpi.edu>
2014-01-08 14:00:37 -0500

    changed build script

--- build_opencv_unix.sh
+++ build_opencv_unix.sh
@@ -1,26 +1,30 @@
 cd ~/code/opencv
-rm -rf ~/code/opencv/build
+# rm -rf ~/code/opencv/build
 mkdir ~/code/opencv/build
 cd ~/code/opencv/build
 
-# locate libpython
-
-# Grab the correct site_packages
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	PYTHON_PACKAGES_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages"
-	# PYTHON_LIBRARY="/opt/local/lib/libpython2.7.dylib"
-	# PYTHON_LIBRARY="/usr/lib/libpython2.7.dylib"
-	PYTHON_LIBRARY="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/libpython2.7.dylib"
-	# PYTHON_LIBRARY=""
-	# PYTHON_LIBRARY=""
-	# PYTHON_LIBRARY=""
-	# PYTHON_LIBRARY=""
+    # IF OXS
+    #Grab the correct site_packages on OSX
+    PYTHON_PACKAGES_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages"
 
-    cmake -G "Unix Makefiles" -D PYTHON_LIBRARY=$PYTHON_LIBRARY -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO .. 
+    cmake -G "Unix Makefiles" \
+        -D CMAKE_BUILD_TYPE=Release \
+        -D BUILD_PERF_TESTS=OFF \
+        -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+        -D BUILD_opencv_legacy=YES \
+    ~/code/opencv
+
+    # Tried: 
+    # PYTHON_LIBRARY="/opt/local/lib/libpython2.7.dylib"
+    # PYTHON_LIBRARY="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/libpython2.7.dylib"
+    # -D PYTHON_LIBRARY=$PYTHON_LIBRARY \
     # -D CMAKE_OSX_ARCHITECTURES=x86_64
     # -D PYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib 
+
 else
-	PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
+    # IF LINUX
+    PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
     cmake -G "Unix Makefiles" -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_legacy=NO ..
 fi
 
