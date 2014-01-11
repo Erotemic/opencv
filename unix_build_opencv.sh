e5a1ca73215e34e147b3c3dee1696168b90545fe
joncrall <crallj@rpi.edu>
2014-01-10 20:49:08 -0500

    wip

--- unix_build_opencv.sh
+++ unix_build_opencv.sh
@@ -1,35 +1,37 @@
+#!/bin/bash
+
 cd ~/code/opencv
-rm -rf ~/code/opencv/build
+#rm -rf ~/code/opencv/build
 mkdir ~/code/opencv/build
 cd ~/code/opencv/build
+#PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
 
-# locate libpython
-
-# Grab the correct site_packages
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	PYTHON_PACKAGES_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages"
-	# PYTHON_LIBRARY="/opt/local/lib/libpython2.7.dylib"
-	# PYTHON_LIBRARY="/usr/lib/libpython2.7.dylib"
-	PYTHON_LIBRARY="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/config/libpython2.7.dylib"
-	# PYTHON_LIBRARY=""
-	# PYTHON_LIBRARY=""
-	# PYTHON_LIBRARY=""
-	# PYTHON_LIBRARY=""
+    # locate python libs
+    PYTHON_LIBRARY="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/
+config/libpython2.7.dylib"
+    #PYTHON_PACKAGES_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages"
+    # PYTHON_LIBRARY="/opt/local/lib/libpython2.7.dylib"
+    # PYTHON_LIBRARY="/usr/lib/libpython2.7.dylib"
+     #-D BUILD_PERF_TESTS=OFF \
 
-    cmake -G "Unix Makefiles" -D PYTHON_LIBRARY=$PYTHON_LIBRARY -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH -D BUILD_PERF_TESTS=OFF -D CMAKE_OSX_ARCHITECTURES=x86_64 ..
-    # -D PYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib 
+    # OXS cmake command
+    export OPENCV_OS_CMAKE_OPTIONS=
+        -D CMAKE_OSX_ARCHITECTURES=x86_64 \
+        -D PYTHON_LIBRARY=$PYTHON_LIBRARY
 else
-	PYTHON_PACKAGES_PATH=$(python -c "import site; print(site.USER_SITE)" 2> /dev/null)
-    cmake -G "Unix Makefiles" \
-        -D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
-        -D BUILD_PERF_TESTS=OFF ..
+    #cmake -G "Unix Makefiles" \
+        #-D PYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+        #-D BUILD_PERF_TESTS=OFF ..
+        #-D WITH_QT=ON \
+        #-D BUILD_opencv_world=ON \
 
-
-    cmake \
-        -D WITH_QT=ON
-        -D INSTALL_PYTHON_EXAMPLES=ON
-        -D BUILD_opencv_world=ON
+    export OPENCV_CMAKE_OPTIONS=-D INSTALL_PYTHON_EXAMPLES=ON
 fi
 
+# Configure
+cmake -G "Unix Makefiles" $OPENCV_OS_CMAKE_OPTIONS ~/code/opencv
+# Build
 make -j9
+# Install
 sudo make install
